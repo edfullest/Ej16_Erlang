64e562cf616b9586b2c066a73fca2fd777f50dbd
Eduardo Serna <lalotahoma@icloud.com>
2017-10-22 23:38:57 -0500

    Finished exercise 4

--- ej1.erl
+++ ej1.erl
@@ -9,9 +9,9 @@
 
 -module(ej1).
 
--export([secuencia/1, mapea/2]).
+-export([secuencia/1, mapea/2, menores/2]).
 
--import(lists, [map/2,foldl/3,foldr/3]).
+-import(lists, [map/2,foldl/3,foldr/3, filter/2]).
 
 % 2
 secuencia ( 0 ) -> [];
@@ -20,3 +20,15 @@ secuencia ( N ) when N > 0 -> secuencia(N - 1) ++ [N].
 % 3
 mapea ( _ , [] ) -> [];
 mapea ( F , [ X | XS ] ) -> [F(X)] ++ mapea(F , XS).
+
+%4
+% menores ([] , _) -> [];
+% menores ([ X | XS] , N) when is_list(X) -> menores(X, N) ++ menores(XS,N);
+% menores ([ X | XS] , N) when is_integer(X), X < N  -> [X] ++ menores(XS, N);
+% menores ([ X | XS] , N) when is_integer(X), X >= N  -> menores(XS, N).
+
+menores ([] , _) -> [];
+menores (L , N) -> Filterfun = fun(LISTA) -> filter(fun(X) -> X < N end, LISTA) end,
+                   Mapfun = fun(LISTA) -> (Filterfun(LISTA)) end,
+                   map (Mapfun , L ). %Mapfun llama a Filterfun, que filtra cada elemento de una lista
+
